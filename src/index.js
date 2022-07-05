7c4ffcda4c6c72476b38b14c7fda792aa224ee4b
Victor Fedorov <vpfedorov@bk.ru>
2022-07-05 21:25:38 +0300

    new route for card info

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // External imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 // Local imports
 import App from "./App";
 import PageNotFound from "./components/UI/PageNotFound/PageNotFound";
 import Character from "./routes/character";
 import Favorites from "./routes/favorites";
+import CharacterInfo from "./components/CharacterInfo";
 
 // for Redux
 import store from './store';
@@ -21,9 +21,13 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
-            <Route index path="/" element={<Character />} />
-            <Route path="/characters" element={<Character />} />
-            <Route path="/favorites" element={<Favorites />} />
+            {/*<Route index path="/" element={<Character />} />*/}
+            <Route path="characters" element={<Outlet />} >
+              <Route index path="" element={<Character />} />
+              <Route path=":id" element={<CharacterInfo />} />
+            </Route>
+
+            <Route path="favorites" element={<Favorites />} />
             <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>

0d895bf83c2941e81aa075bd6044d8c5f4b2ef4f
Victor Fedorov <vpfedorov@bk.ru>
2022-07-06 21:30:36 +0300

    Menu component initial version

---
+++ src/components/UI/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <ul className="characters__categories">
+                <li className="character__type">All</li>
+                <li className="character__type">Human</li>
+                <li className="character__type">Animal</li>
+                <li className="character__type">Alien</li>
+            </ul>
+
+            <form action="" method="get" className="formSearchCharacter">
+                <input type="text" name="name" id="name" required placeholder="Search by name..." />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    );
+};
+
+export default Menu;

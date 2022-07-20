131a65d0e05064f4301e0c888a51aac2c00b5de2
Victor Fedorov <vpfedorov@bk.ru>
2022-07-21 00:04:37 +0300

    get Characters by Type

--- src/components/UI/Menu/Menu.js
+++ src/components/UI/Menu/Menu.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
+import {
+    addSelectedCharacters,
+    setNextPage,
+    setPrevPage
+} from "../../../features/selectedCharacters/selectedCharactersSlice";
+import {useDispatch} from "react-redux";
+import APIClient from "../../../services/APIClient";
 import './Menu.css';
 
 const Menu = () => {
+    const dispatch = useDispatch();
+    const client = new APIClient();
+
+    const getCharactersType = (event) => {
+        const characterType = event.target.className.split(" ")[1];
+
+        client.fetchCharactersBySpecies(characterType).then((result) => {
+            console.log(result.data);
+            dispatch(addSelectedCharacters(result.data.results));
+            dispatch(setNextPage(result.data.info.next))
+            dispatch(setPrevPage(result.data.info.prev))
+        });
+    }
+
     return (
         <div className="menu">
             <ul className="characters__categories">
-                <li className="character__type">All</li>
-                <li className="character__type">Human</li>
-                <li className="character__type">Animal</li>
-                <li className="character__type">Alien</li>
+                <li className="character__type All" onClick={getCharactersType}>All</li>
+                <li className="character__type Human" onClick={getCharactersType}>Human</li>
+                <li className="character__type Animal" onClick={getCharactersType}>Animal</li>
+                <li className="character__type Alien" onClick={getCharactersType}>Alien</li>
             </ul>
 
             <form action="" method="get" className="formSearchCharacter">
